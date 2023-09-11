e66c4266527244dcd08077c0c3e8f7c11dae825b
alexiasch <alexiasch@protonmail.com>
2023-09-11 23:01:57 +0200

    nom standard pour corpus eval

--- config.py
+++ config.py
@@ -8,8 +8,9 @@
     airs_ref : path linking to the document containing the titles of the couplets corrected manually
     characters_sheet : path linking to the csv containing all the characters annotated manually
     """
-
-dossier = "../thealtres-ocr/corpus-items"
+# corpus habituel :
+# dossier = ../thealtres-ocr/corpus-items
+dossier = "corpus-evaluation"
 airs_ref = "data/liste_des_noms_d-airs_standards.txt"
 characters_sheet = f"data/annotations_fr-characters.csv"
 dossier_out = "out"
