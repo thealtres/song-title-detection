66e246c361f37d5752d3ae47f2dc5677583d8d8c
alexiasch <alexiasch@protonmail.com>
2023-09-14 23:01:37 +0200

    nettoyage des modules d'options

--- encoding.py
+++ encoding.py
@@ -10,19 +10,16 @@ import os.path
 
 #encodage par concaténation:
 def encode_air(id_work):
-    """ Ecriture depuis le text brut d'un document xml avec les élément <stage> correspondant aux airs sélectionnés dans le mode extraction.
+    """ 
+    Ecriture depuis le text brut d'un document xml avec les élément <stage> correspondant aux airs sélectionnés dans le mode extraction.
     """
+    doc_entree = f"{dossier}/{id_work}/{id_work}_03_all-text_tesseract.txt"
+    if not os.path.exists(doc_entree):
+        doc_entree = f"{dossier}/{id_work}/{id_work}_00_all-text_original-ocr.txt"
     dossier_id = f"{dossier}/{id_work}"
-    #doc_sortie = f"{dossier_id}/encodage-airs/{id_work}_airs-encodes.xml"
-    #doc_air = f"{dossier_id}/{str(id_work)}_airs.csv"
-    #for testing purposes:
-    doc_air = "01_airs.csv"
-    doc_txt = "01_03_all-text_tesseract.txt"
-    doc_sortie = f"{id_work}_airs-encodes.xml"
-    #docs_txt = [file for file in glob.glob(f"{dossier_id}/*.txt") \
-        #if os.path.basename(file).endswith("tesseract.txt")]
-    #for doc_entree in docs_txt: 
-    with open(doc_txt, "r", encoding="utf8") as f1,\
+    doc_sortie = f"{dossier_id}/05_tune_names/{id_work}_airs-encodes.xml"
+    doc_air = f"{dossier_id}/05_tune_names/{str(id_work)}_airs.csv"
+    with open(doc_entree, "r", encoding="utf8") as f1,\
         open(doc_air, "r", encoding="utf-8") as f2,\
         open(doc_sortie, "w", encoding="utf-8") as f3:
         airs = []
@@ -45,37 +42,37 @@ def encode_air(id_work):
                 f3.write(str(line) + "\n")
         f3.write(''''\n\t</body>\n</text>''')
 
-#encodage par construction d'un arbre lxml
-def encode(id_work):
-    #dossier_id = f"{dossier}/{id_work}"
-    #doc_sortie = f"{dossier_id}/{id_work}_airs-encodes.xml"
-    #doc_air = f"{dossier_id}/{str(id_work)}_airs.csv"
-    #for testing purposes:
-    doc_air = "test/01_airs.csv"
-    doc_txt = "test/01_03_all-text_tesseract.txt"
-    doc_sortie = f"test/{id_work}_airs-encodes.xml"
-    #docs_txt = [file for file in glob.glob(f"{dossier_id}/*.txt") \
-        #if os.path.basename(file).endswith("tesseract.txt")]
-    #for doc_entree in docs_txt: 
-    with open(doc_txt, "r", encoding="utf8") as f1,\
-        open(doc_air, "r", encoding="utf-8") as f2,\
-        open(doc_sortie, "wb") as f3:
-        airs = []
-        airs_id = []
-        for line in f2:
-            colonne = line.rstrip().split(";")
-            if "=" in colonne[2]:
-                airs_id.append(colonne[2].split("=")[1])
-            else:
-                airs.append(colonne[2])
-        poem = etree.Element("poem")
-        body = etree.SubElement(poem, "body")
-        text_ligne = [line.rstrip() for line in f1]
-        body = E("body", [str(l) if l.strip() not in airs else E("stage", str(l)) for l in text_ligne ])
-        f3.write(etree.tostring(poem, pretty_print=True, xml_declaration=True))
+# #encodage par construction d'un arbre lxml
+# def encode(id_work):
+#     #dossier_id = f"{dossier}/{id_work}"
+#     #doc_sortie = f"{dossier_id}/{id_work}_airs-encodes.xml"
+#     #doc_air = f"{dossier_id}/{str(id_work)}_airs.csv"
+#     #for testing purposes:
+#     doc_air = "test/01_airs.csv"
+#     doc_txt = "test/01_03_all-text_tesseract.txt"
+#     doc_sortie = f"test/{id_work}_airs-encodes.xml"
+#     #docs_txt = [file for file in glob.glob(f"{dossier_id}/*.txt") \
+#         #if os.path.basename(file).endswith("tesseract.txt")]
+#     #for doc_entree in docs_txt: 
+#     with open(doc_txt, "r", encoding="utf8") as f1,\
+#         open(doc_air, "r", encoding="utf-8") as f2,\
+#         open(doc_sortie, "wb") as f3:
+#         airs = []
+#         airs_id = []
+#         for line in f2:
+#             colonne = line.rstrip().split(";")
+#             if "=" in colonne[2]:
+#                 airs_id.append(colonne[2].split("=")[1])
+#             else:
+#                 airs.append(colonne[2])
+#         poem = etree.Element("poem")
+#         body = etree.SubElement(poem, "body")
+#         text_ligne = [line.rstrip() for line in f1]
+#         body = E("body", [str(l) if l.strip() not in airs else E("stage", str(l)) for l in text_ligne ])
+#         f3.write(etree.tostring(poem, pretty_print=True, xml_declaration=True))
 
 
 if __name__ == '__main__':
-    idWork = "01"
-    encode(idWork)
-    #encode_air(idWork)
+    idWork = "001"
+    # encode(idWork)
+    encode_air(idWork)
--- semantic_search.py
+++ semantic_search.py
@@ -11,57 +11,19 @@ bi_encoder = SentenceTransformer('distiluse-base-multilingual-cased-v1')
 bi_encoder.max_seq_length = 256     #Truncate long passages to 256 tokens
 top_k = 32                          #Number of passages we want to retrieve with the bi-encoder
 
-#The bi-encoder will retrieve 100 documents. We use a cross-encoder, to re-rank the results list to improve the quality
-
-#cross_encoder = CrossEncoder('amberoad/bert-multilingual-passage-reranking-msmarco')#
 corpus_embeddings = bi_encoder.encode(list(std_airs), convert_to_tensor=True, show_progress_bar=True)
 std_airs = list(std_airs)
 
-# This function will search all wikipedia articles for passages that
-# answer the query
+# This function will search all wikipedia articles for passages that answer the query
 def search_simple(query):
-    #print("Input question:", query)
-
-    ##### BM25 search (lexical search) #####
-    #bm25_scores = bm25.get_scores(bm25_tokenizer(query))
-    #top_n = np.argpartition(bm25_scores, -5)[-5:]
-    #bm25_hits = [{'corpus_id': idx, 'score': bm25_scores[idx]} for idx in top_n]
-    #bm25_hits = sorted(bm25_hits, key=lambda x: x['score'], reverse=True)
-
-    #print("Top-3 lexical search (BM25) hits")
-    #for hit in bm25_hits[0:5]:
-    #    print("\t{:.3f}\t{}".format(hit['score'], std_airs[hit['corpus_id']].replace("\n", " ")))
-
     ##### Semantic Search #####
     # Encode the query using the bi-encoder and find potentially relevant passages
     question_embedding = bi_encoder.encode(query, convert_to_tensor=True)
     #question_embedding = question_embedding.cuda
-    #top_k = 32
     hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=top_k)
     hits = hits[0]  # Get the hits for the first query
-
-    ##### Re-Ranking #####
-    # Now, score all retrieved passages with the cross_encoder
-    #cross_inp = [[query, std_airs[hit['corpus_id']]] for hit in hits]
-    #cross_scores = cross_encoder.predict(cross_inp)
-
-    # Sort results by the cross-encoder scores
-    #for idx in range(len(cross_scores)):
-        #hits[idx]['cross-score'] = cross_scores[idx]
-
-    # Output of top-5 hits from bi-encoder
-    #print("\n-------------------------\n")
-    #print("Top-3 Bi-Encoder Retrieval hits")
+    res = []
     hits = sorted(hits, key=lambda x: x['score'], reverse=True)
-    #for hit in hits[0:5]:
-        #res.append(std_airs[hit['corpus_id']])
-        #print("\t{:.3f}\t{}".format(hit['score'], std_airs[hit['corpus_id']].replace("\n", " ")))
-    res = [std_airs[hit['corpus_id']] for hit in hits[0:3]]
-    #return std_airs[hits[0]['corpus_id']], std_airs[hits[1]['corpus_id']], std_airs[hits[2]['corpus_id']], std_airs[hits[3]['corpus_id']], std_airs[hits[4]['corpus_id']]
+    for hit in hits[0:5]:
+        res.append((std_airs[hit['corpus_id']].replace("\n", " "), round(float(hit['score'])*100, 2)))
     return res
-    # Output of top-5 hits from re-ranker
-   # print("\n-------------------------\n")
-    #print("Top-3 Cross-Encoder Re-ranker hits")
-    #hits = sorted(hits, key=lambda x: x['cross-score'][1], reverse=True)
-    #for hit in hits[0:5]:
-        #print("\t{:.3f}\t{}".format(hit['cross-score'][1], std_airs[hit['corpus_id']].replace("\n", " ")))
